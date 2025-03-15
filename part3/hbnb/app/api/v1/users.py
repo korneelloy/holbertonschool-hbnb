d4ccea5bb2d47394f50a7f990fe492e571706d11
antoninl94 <9803@holbertonstudents.com>
2025-03-15 10:28:03 +0100

    hotfix SQL file

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -69,7 +69,8 @@ class UserResource(Resource):
             'id': user.id,
             'first_name': user.first_name,
             'last_name': user.last_name,
-            'email': user.email
+            'email': user.email,
+            'password': user.password
             }, 200
 
 
--- part3/hbnb/database.sql
+++ part3/hbnb/database.sql
@@ -1,37 +1,46 @@
 CREATE TABLE IF NOT EXISTS `users` (
     id CHAR(36) PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    is_admin BOOLEAN DEFAULT FALSE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    _first_name VARCHAR(255),
+    _last_name VARCHAR(255),
+    _email VARCHAR(255),
+    _password VARCHAR(255),
+    _is_admin BOOLEAN DEFAULT FALSE
 );
  
 CREATE TABLE IF NOT EXISTS `places` (
     id CHAR(36) PRIMARY KEY,
-    title VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    _title VARCHAR(255),
     description TEXT,
-    price DECIMAL(10,2),
-    latitude FLOAT,
-    longitude FLOAT,
-    owner_id CHAR(36), FOREIGN KEY (owner_id) REFERENCES `users`(id)
+    _price DECIMAL(10,2),
+    _latitude FLOAT,
+    _longitude FLOAT,
+    _owner_id CHAR(36), FOREIGN KEY (_owner_id) REFERENCES `users`(id)
 );
  
 CREATE TABLE IF NOT EXISTS `reviews` (
     id CHAR(36) PRIMARY KEY,
-    text TEXT,
-    rating INT CHECK (rating BETWEEN 1 AND 5),
-    user_id CHAR(36),
-    place_id CHAR(36),
-    FOREIGN KEY (user_id) REFERENCES `users`(id),
-    FOREIGN KEY (place_id) REFERENCES `places`(id),
-    UNIQUE (user_id, place_id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    _rating INT CHECK (_rating BETWEEN 1 AND 5),
+    _comment TEXT,
+    _place_id CHAR(36),
+    _user_id CHAR(36),
+    FOREIGN KEY (_user_id) REFERENCES `users`(id),
+    FOREIGN KEY (_place_id) REFERENCES `places`(id),
+    UNIQUE (_user_id, _place_id)
 );
 
 
 CREATE TABLE IF NOT EXISTS `amenities` (
     id CHAR(36) PRIMARY KEY,
-    name VARCHAR(255) UNIQUE
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    _name VARCHAR(255) UNIQUE,
+    _description TEXT
 );
 
 
@@ -43,11 +52,11 @@ CREATE TABLE IF NOT EXISTS `amenity_place` (
 );
 
 
-INSERT INTO `users` (id, email, first_name, last_name, password, is_admin)
-VALUES ('36c9050e-ddd3-4c3b-9731-9f487208bbc1', 'admin@hbnb.io', 'Admin', 'HBnB', 'Admin1234', TRUE);
+INSERT INTO `users` (id, _email, _first_name, _last_name, _password, _is_admin)
+VALUES ('36c9050e-ddd3-4c3b-9731-9f487208bbc1', 'admin@hbnb.io', 'Admin', 'HBnB', '$2b$12$HRaI5zBYJBdazSF8lNFRdecX.lDUmZ0wFSyIF/jMTq.uKHoo27xEq', TRUE);
 
-INSERT INTO `amenities` (id, name)
+INSERT INTO `amenities` (id, _name, _description)
 VALUES  
-    (lower(hex(randomblob(16))), 'Wifi'),
-    (lower(hex(randomblob(16))), 'Swimming Pool'),
-    (lower(hex(randomblob(16))), 'Air Conditioning');
+    (lower(hex(randomblob(16))), 'Wifi', 'Yahoo there is WiFi'),
+    (lower(hex(randomblob(16))), 'Swimming Pool', 'Swimming is good, drowning is blblbllbl'),
+    (lower(hex(randomblob(16))), 'Air Conditioning', "It's fresh in here!");
